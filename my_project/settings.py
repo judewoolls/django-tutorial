dd3ce990eb3d7bc0879ef0b7e494f9938ac7e1ef
Jude Woolls <jwoolls01@gmail.com>
2024-12-20 16:20:43 +0000

    set debug to true

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v48caj1g1o)mzxqo22q5(428^mojudy4wxv%j!6ocjdx0&fa!='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
